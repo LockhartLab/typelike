48908b8ef59a29b4ae3f8dee6ebdecdb870ce7c1
doclockh <chris@lockhartlab.org>
2019-12-23 14:16:15 -0800

    Fixing __init__

--- typelike/__init__.py
+++ typelike/__init__.py
@@ -1,16 +1,9 @@
 
-from . import core
 
 from .core import *
 from .version import __version__
 
-# Contents
-# __all__ = ['__version__']
-# __all__ += core.__all__
 __all__ = [
-    'AnyLike',
-    'Anything',
-    'ListLike',
-    'NumberLike',
+    'core',
     '__version__'
 ]
--- typelike/core/__init__.py
+++ typelike/core/__init__.py
@@ -1,12 +1,11 @@
 
-from . import any
-from . import list
-from . import number
-
 from .any import *
 from .list import *
 from .number import *
 
-__all__ = any.__all__
-__all__ += list.__all__
-__all__ += number.__all__
+__all__ = [
+    'AnyLike',
+    'Anything',
+    'ListLike',
+    'NumberLike'
+]
